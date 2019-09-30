c5f185329888a58dd19c4ff598e5e5fab4aaf5f1
Maria Palacios <imarip09@gmail.com>
2019-09-30 16:15:49 -0400

    added express parser to server file

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
 );
 
-app.get('/api/*', (req, res) => {
-  res.json({ok: true});
+app.get('/api/', (req, res) => {
+  res.send('Hello world!');
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
