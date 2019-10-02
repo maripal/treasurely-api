6ccd0e2f6ea3af4ad5b790f9af11bb74842564c4
Maria Palacios <imarip09@gmail.com>
2019-10-02 14:51:12 -0400

    added user reference to item

--- models/items.js
+++ models/items.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   name: { type: String, required: true },
   price: { type: Number, required: true },
   purchased: Boolean
