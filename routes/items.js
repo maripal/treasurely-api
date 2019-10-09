2d7a145e9573ca99796530000fa2dd7b38ac7307
Maria Palacios <imarip09@gmail.com>
2019-10-09 15:32:05 -0400

    fixed get route & added post endpoint

--- routes/items.js
+++ routes/items.js
@@ -3,11 +3,22 @@ const router = express.Router();
 let Item = require('../models/items');
 
 router.route('/').get((req, res) => {
-  Item.find
+  Item.find() 
   .then(items => res.status(200).json(items))
   .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
+router.route('/add').post((req, res) => {
+  const { name, price } = req.body;
 
+  const newItem = new Item({
+    name,
+    price
+  });
+
+  newItem.save()
+  .then(() => res.status(201).json(`Item added!`))
+  .catch(err => res.status(400).json(`Error: ${err}`));
+});
 
 module.exports = router;
