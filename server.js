453e94c2a47fc562ae45f529098fd54f593b0678
Maria Palacios <imarip09@gmail.com>
2020-05-03 14:57:49 -0500

    fixed file

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 
 app.use('/users', usersRouter);
