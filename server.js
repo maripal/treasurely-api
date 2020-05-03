5d1974b298ab89ef3e774353bf32e6513c836eee
Maria Palacios <imarip09@gmail.com>
2020-05-03 18:34:35 -0500

    fixed files

--- server.js
+++ server.js
@@ -21,16 +21,6 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 
 app.use('/users', usersRouter);
 app.use('/items', itemsRouter);
@@ -39,9 +29,9 @@ app.use('/auth', authRouter);
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
-// app.get('/', (req, res) => {
-//   res.json({ message: 'Hello world!' });
-// });
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello world!' });
+});
 
 let server;
 
